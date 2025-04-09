f0d2c5bb54ad053023538dddb70cbeae1fcf293d
Vardan <vardhan.boya24@ngit.edu.in>
2025-04-09 22:14:51 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ const Home: NextPage = () => {
   const fetchData = async () => {
     setRefreshing(true);
     try {
-      const response = await fetch('http://iot25.vercel.app/api/data');
+      const response = await fetch('https://iot25.vercel.app/api/data');
       if (!response.ok) throw new Error('Failed to fetch data');
       const data = await response.json();
       setHealthData(data);
